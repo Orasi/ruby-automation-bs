a734082fd869b38213cf6db467a4f9594452a2db
KevinMunro <Kevin.Munro@orasi.com>
2014-07-11 08:43:00 -0700

    Working expect for hidden column issue

--- features/step_definitions/sort_by_employee_steps.rb
+++ features/step_definitions/sort_by_employee_steps.rb
@@ -1,38 +1,38 @@
 Given (/The employee page/i) do
 	@browser.goto 'https://bluesourcestaging.herokuapp.com'
+	@browser.link(text: "Can't log in?").wait_until_present
+	@browser.text_field(id: 'employee_username').set "company.admin"
+	@browser.text_field(id: 'employee_password').set "password"
+	@browser.button(value: 'Login').click
 end
 
-When (/I sort by (.*) in (.*) order/i) do |sortby| |order|
+When (/I sort by (.*) in (.*) order/i) do |sortby, order|
 	@browser.table(class: 'table').wait_until_present
 	@browser.link(text: /#{sortby}/i).click
 	if order == "ascending"
-        	{
-        	@browser.link(text: /#{sortby}/i).click
-        	}
+        		@browser.link(text: /#{sortby}/i).click        	
 	end
 end
 
-Then (/Then the "(.*)" column should be sorted in "(.*)" order /) do |sortby| |order|
-	rows=@browser.table(class: "table").rows.lengh
+Then (/the (.*) column should be sorted in (.*) order/i) do |sortby, order|
+	rows=@browser.table(class: "table").rows.length
 	headers=@browser.table(class: "table").tr.ths.collect { |x| x.text.downcase }
-	column_index=headers.index(/#{sortby}/i)
-	if order="ascending"	
-		for i in rows-1
+	column_index=headers.index(sortby.downcase)
+	if order=="ascending"
+		i=1	
+		while (i<(rows-1))
 			cell_text = @browser.table(class: 'table')[i][column_index].text
 			next_cell_text = @browser.table(class: 'table')[i+1][column_index].text
-		        if cell_text >= next_cell_text
-				return false
-			end
+			expect(@browser.table(class: 'table')[i][column_index].text).to be <= @browser.table(class: 'table')[i+1][column_index].text	
+			i=i+1
 		end
-        return true
-	elseif
-		for i in rows-1
+	else
+		i=1
+		while (i<(rows-1))
 			cell_text = @browser.table(class: 'table')[i][column_index].text
 			next_cell_text = @browser.table(class: 'table')[i+1][column_index].text
-			if next_cell_text >= cell_text
-				return false
-			end
+			expect(@browser.table(class: 'table')[i+1][column_index].text).to be <= @browser.table(class: 'table')[i][column_index].text
+			i=i+1
 		end
-	return true
 	end	
 end
