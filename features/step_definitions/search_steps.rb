7021bf823f4eee5d28a995ce1a114639562f4e25
Lew Gordon <lgordon2@gmail.com>
2014-07-21 20:05:09 -0400

    More code cleanup and hopefully a travis fix.

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,17 +1,13 @@
 When(/I search for an employee by (?:their )?(\w+\s*\w+)$/) do |criteria|
   @browser.table(class: 'table').wait_until_present
-  if criteria.split(' ').count > 1
-    @criteria = criteria.split(' ').join(' ')
-  else
-    @criteria = criteria
-  end
+  @criteria = criteria.tr_s(' ', '_').to_sym
   @browser.text_field(id: 'search-bar').set @employee[@criteria.to_sym]
 end
 
 When(/I search for an employee by only a portion of their name/) do
   @browser.table(class: 'table').wait_until_present
-  @criteria = 'first_name'
-  @search_for = @employee[@criteria.to_sym][0..3]
+  @criteria = :first_name
+  @search_for = @employee[@criteria][0..3]
   @browser.text_field(id: 'search-bar').set @search_for
 end
 
@@ -23,7 +19,7 @@ Then(/the results should show the employees fitting the search criteria/) do
   if !@search_for
     search_results = @browser
                       .table(class: 'table')
-                      .trs(text: /#{@employee[@criteria.to_sym]}/i).count
+                      .trs(text: /#{@employee[@criteria]}/i).count
   else
     search_results = @browser
                       .table(class: 'table')
