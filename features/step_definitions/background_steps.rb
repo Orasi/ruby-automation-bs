6162a8b010d0cba19ec56873836d170ce4d801e6
David Quach <davidnquach@gmail.com>
2014-07-29 12:11:56 -0400

    Fix style with Rubocop

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -7,13 +7,13 @@ Given(/that I am logged in as a (\w+\s*\w+)/) do |user|
 end
 
 And(/I navigated? to the (\w+\s*\w*) page/) do |page|
-	if page.split(" ").length > 1 
-		page = page.split(" ")
-		main_link = page[0]
-		sub_link = page[1]
-		@browser.link(href: '#', text: main_link).click
-		@browser.link(text: /#{sub_link}/i).click
-	else
-		@browser.link(text: /#{page}/i).click
-	end
+  if page.split(' ').length > 1
+    page = page.split(' ')
+    main_link = page[0]
+    sub_link = page[1]
+    @browser.link(href: '#', text: main_link).click
+    @browser.link(text: /#{sub_link}/i).click
+  else
+    @browser.link(text: /#{page}/i).click
+  end
 end
