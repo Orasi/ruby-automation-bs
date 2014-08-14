4952ebe57d418a1824942211332e978ab9cef7bc
Damien Storm <damien.storm@orasi.com>
2014-08-14 16:50:41 +0000

    added wait for email button, changed description for email button, added pending for next step

--- features/step_definitions/email_team.rb
+++ features/step_definitions/email_team.rb
@@ -1,5 +1,7 @@
 When(/I select the email team button/i) do
-  @browser.button(value: 'Email Team').click
+  @browser.element(text: 'Email team').wait_until_present
+  @browser.element(text: 'Email team').click
 end
 Then(/I should be able to send an email to my active direct reports/i) do
+    pending "Need to determine how this verification will be done"
 end
