583d01c93ce52dabb49a8ed0c18229ad96e4331a
Damien Storm <damien.storm@orasi.com>
2014-08-14 18:10:23 +0000

    rubocop fixes

--- features/step_definitions/email_team.rb
+++ features/step_definitions/email_team.rb
@@ -3,5 +3,5 @@ When(/I select the email team button/i) do
   @browser.element(text: 'Email team').click
 end
 Then(/I should be able to send an email to my active direct reports/i) do
-    pending "Need to determine how this verification will be done"
+  pending 'Need to determine how this verification will be done'
 end
