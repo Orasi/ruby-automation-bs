9b0746a0a7c3bf09a499ab1a7641a471c783a0e2
ryderstorm <damien.r.storm@gmail.com>
2014-08-14 13:47:17 +0000

    moved login functionality to separate function 'login_as_user' and added the other login step definition

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -1,9 +1,9 @@
 Given(/that I am logged in as a (\w+\s*\w+)/) do |user|
-  @browser.goto 'https://bluesourcestaging.herokuapp.com'
-  user = user.split(' ').join('.')
-  @browser.text_field(id: 'employee_username').set user
-  @browser.text_field(id: 'employee_password').set 'password'
-  @browser.button(value: 'Login').click
+  login_as_user(user)
+end
+
+Given(/that I am logged in as (\w+\s*\w+)/) do |user|
+  login_as_user(user)
 end
 
 And(/I navigated to the (\w*) page/) do |page|
@@ -16,4 +16,26 @@ end
 
 And(/I navigate to the (\w*) page/) do |page|
   @browser.element(class: 'navbar').link(text: /#{page}/i).click
+end
+
+def login_as_user(user)
+  case user
+  when "user"
+    user = "test.user"
+  when "manager"
+    user = "david.quach"
+  when "dept head"
+    user = "dept.head"
+  when "dept admin"
+    user = "dept.admin"
+  when "company admin"
+    user = "company.admin"
+  else
+    user = user.split(' ').join('.')
+  end
+    
+  @browser.goto 'https://bluesourcestaging.herokuapp.com'
+  @browser.text_field(id: 'employee_username').set user
+  @browser.text_field(id: 'employee_password').set 'password'
+  @browser.button(value: 'Login').click
 end
