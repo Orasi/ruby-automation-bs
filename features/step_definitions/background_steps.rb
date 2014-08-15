fe2e752d2aa44c491f2208cddeb1223e274e4386
Damien Storm <damien.storm@orasi.com>
2014-08-15 14:34:39 +0000

    added placeholder function calls for taking screenshots during login process to debug issues

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -33,10 +33,10 @@ def login_as_user(user)
   @browser.goto 'https://bluesourcestaging.herokuapp.com'
   @browser.text_field(id: 'employee_username').set user
   @browser.text_field(id: 'employee_password').set 'password'
-  takescreenshot('login_with_[#{user}]_before')
+  # takescreenshot('login_with_['+user+']_before')
   @browser.button(value: 'Login').click
   @browser.element(text: 'Logout').wait_until_present(10)
-  takescreenshot('login_with_[#{user}]_after')
+  # takescreenshot('login_with_['+user+']_after')
 end
 # rubocop:enable Style/MethodLength
 
