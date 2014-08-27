f451153e4d0845ac0b1fa2dcfda271aa0abd807c
Damien Storm <damien.storm@orasi.com>
2014-08-27 12:53:53 +0000

    udpates to avoid conflicts while merging back to master

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -2,16 +2,16 @@ Given(/that I am logged in as (.*)/i) do |user|
   login_as_user(user)
 end
 
-And(/I navigated to the (\w*) page/) do |page|
-  @browser.element(class: 'navbar').link(text: /#{page}/i).click
-end
-
-Given(/I navigate to the (\w*) page/) do |page|
-  @browser.element(class: 'navbar').link(text: /#{page}/i).click
-end
-
-And(/I navigate to the (\w*) page/) do |page|
-  @browser.element(class: 'navbar').link(text: /#{page}/i).click
+And(/I navigated? to the (\w+\s*\w*) page/) do |page|
+  if page.split(' ').length > 1
+    page = page.split(' ')
+    main_link = page[0]
+    sub_link = page[1]
+    @browser.link(href: '#', text: main_link).click
+    @browser.link(text: /#{sub_link}/i).click
+  else
+    @browser.link(text: /#{page}/i).click
+  end
 end
 
 # rubocop:disable Style/MethodLength
