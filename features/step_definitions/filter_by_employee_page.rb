999f78b58e7633428ca1c2e82ccbbf716ea9fac2
Damien Storm <damien.storm@orasi.com>
2014-09-10 16:38:12 +0000

    added pending step for inactive imployees, udpated direct reports step to use goto_next_page, added all employees step

--- features/step_definitions/filter_by_employee_page.rb
+++ features/step_definitions/filter_by_employee_page.rb
@@ -3,9 +3,18 @@ When(/^I filter using the all button$/) do
   @browser.table(class: 'table').wait_until_present(5)
 end
 
+Then(/^the employee table should show all employees$/) do
+  total_employees =  @browser.p(class: 'pull-right ng-binding').text.split[4].to_i
+  expect(verify_table_count).to eq(total_employees)
+end
+
 When(/^I filter using the show inactives button$/) do
   @browser.element(class: 'btn', text: 'Show Inactives').click
   @browser.table(class: 'table').wait_until_present(5)
+end
+
+Then(/^the employee table should show only the inactive employees$/) do
+  pending # This step is on hold because the functionality isn't implemented on the website yet.
 end
 
 When(/^I filter using the direct button$/) do
@@ -14,10 +23,14 @@ When(/^I filter using the direct button$/) do
 end
 
 Then(/^the employee table should show only direct reports$/) do
+  @browser.select_list(id: 'employee_preferences_resourcesPerPage').select_value('20')
   expected_supervisor = @user[:first] + ' ' + @user[:last]
-  table = @browser.table(class: 'table')
-  (1..table.rows.count - 1).each_with_index do |i|
-    actual_supervisor = table[i][3].text
-    expect(actual_supervisor).to eq(expected_supervisor)
+  while goto_next_page
+    table = @browser.table(class: 'table')
+    (1..table.rows.count - 1).each_with_index do |i|
+      actual_supervisor = table[i][3].text
+      expect(actual_supervisor).to eq(expected_supervisor)
+    end
   end
 end
+
