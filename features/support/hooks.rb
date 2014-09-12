d0fc5c3b8c780e5c5a48d0bf61dba1aa7e333d7b
Damien Storm <damien.storm@orasi.com>
2014-09-12 15:28:03 +0000

    fix for embedded screenshots, removed | from filename generators

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -32,9 +32,11 @@ After do |scenario|
       info = "#{title}_#{name}_#{example}"
     end
     time = Time.now.strftime('%Y%m%d_%H%M%S')
-    screenshot = "./screenshots/#{time}|FAILED|#{info}.png"
+    screenshot = "./screenshots/#{time}--FAILED--#{info}.png"
     @browser.screenshot.save screenshot
-    embed screenshot, 'image/png'
+    # embed screenshot, 'image/png'
+    encoded_img = @browser.driver.screenshot_as(:base64)
+    embed("data:image/png;base64,#{encoded_img}", 'image/png')
   end
 end
 
