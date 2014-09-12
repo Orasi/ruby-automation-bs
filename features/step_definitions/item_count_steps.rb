84b1cc6522926708b04bb519dadf8b291fa97703
Damien Storm <damien.storm@orasi.com>
2014-09-12 17:16:29 +0000

    change time out from default of 5 to 10 seconds

--- features/step_definitions/item_count_steps.rb
+++ features/step_definitions/item_count_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then(/the employee table should show the first (\d+) employees/) do |pages|
   pages = pages.to_i
-  row_count = employee_page.employee_table_element.when_visible.rows - 1
+  row_count = employee_page.employee_table_element.when_visible(10).rows - 1
   if employee_page.total_employee_results >= pages
     expect(row_count).to eq(pages)
   else
