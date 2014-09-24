e85ca15f8bdb0815d960f315d97c1a3f131fa2ec
Damien Storm <damien.storm@orasi.com>
2014-09-24 20:18:26 +0000

    updated email_team object, added direct object, added email_method

--- features/page_objects/employee_page.rb
+++ features/page_objects/employee_page.rb
@@ -5,11 +5,16 @@ class EmployeePage
   # Objects on the page
   select_list(:employee_per_page, id: 'employee_preferences_resourcesPerPage')
   table(:employee_table, class: 'table-bordered')
-  link(:email_team, text: 'Email team')
+  div(:email_team, text: 'Email team')
+  label(:direct, text: 'Direct')
   paragraph(:results, class: 'ng-binding')
 
   # Methods done on the objects
   def total_employee_results
     results.split(' of ').last.to_i
+  end
+
+  def email_list
+    email_team_element.link_element.href.sub('mailto:', '').split(';')
   end
 end
