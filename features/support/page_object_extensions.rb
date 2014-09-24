da77cf013306d640e708bbf92c652e3be16dd8c8
Damien Storm <damien.storm@orasi.com>
2014-09-24 20:32:37 +0000

    updated console output for command to reflect changes to command

--- features/support/page_object_extensions.rb
+++ features/support/page_object_extensions.rb
@@ -16,6 +16,10 @@ module PageObjectHelpers
     @employee_page ||= EmployeePage.new(@browser)
   end
 
+  def employee_details_page
+    @employee_details_page ||= EmployeeDetailsPage.new(@browser)
+  end
+
   def title_page
     @title_page ||= TitlePage.new(@browser)
   end
